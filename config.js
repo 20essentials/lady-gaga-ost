08dde8fd2fc888ed2cf1d3bbc47deed186bd8e4e
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:31:44 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LADY GAGA ';
+export let nameSongs = [
+	"JUST DANCE", /*1*/
+	"POKER FACE", /*2*/
+	"BAD ROMANCE", /*3*/
+	"PAPARAZZI", /*4*/
+	"JUDAS", /*5*/
+	"BLOODY MARY ", /*6*/
+	"LOVEGAME", /*7*/
+	"ALEJANDRO", /*8*/
+	"THE EDGE OF GLORY", /*9*/
+	"BORN THIS WAY", /*10*/
+	"APPLAUSE", /*11*/
+	"JUDAS - VERSION GUITAR", /*12*/
+	"JUDAS - 80S VERSION", /*13*/
+	"TELEPHONE", /*14*/
+	"JUDAS (SLOWED + REVERB)", /*15*/
+	"G.U.Y.", /*16*/
+	"JUDAS ( GEMYNI COVER + SLOWED REVERB )", /*17*/
+	"PERFECT ILLUSION", /*18*/
+	"HEAVY METAL LOVER", /*19*/
+	" YOU AND I", /*20*/
+]
